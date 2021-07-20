cf1b9b0d7dae19f9053ff5a8075201ae40fe78af
mkdata95 <mkdata95@naver.com>
2021-07-20 16:51:22 +0900

    DB 업로드 완성

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -3,7 +3,7 @@ import Dropzone from "react-dropzone";
 import { Icon } from "antd";
 import axios from "axios";
 
-function FileUpload() {
+function FileUpload(props) {
   const [Images, setImages] = useState([]);
 
   const dropHandler = (files) => {
@@ -14,12 +14,22 @@ function FileUpload() {
     formData.append("file", files[0]);
     axios.post("/api/product/image", formData, config).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
+        // console.log(response.data);
         setImages([...Images, response.data.filePath]);
+        props.refreshFunchtion([...Images, response.data.filePath]);
       } else {
         alert("파일 저장하는데 실패하였습니다.");
       }
     });
+  };
+
+  const deleteHandler = (image) => {
+    const currentIndex = Images.indexOf(image);
+    // console.log("currentIndex", currentIndex); 삭제 확인용
+    let newImages = [...Images];
+    newImages.splice(currentIndex, 1);
+    setImages(newImages);
+    props.refreshFunchtion(newImages);
   };
 
   return (
@@ -52,7 +62,7 @@ function FileUpload() {
         }}
       >
         {Images.map((image, index) => (
-          <div key={index}>
+          <div onClick={() => deleteHandler(image)} key={index}>
             <img
               style={{ minWidth: "300px", width: "300px", height: "240px" }}
               src={`http://localhost:5000/${image} `}
