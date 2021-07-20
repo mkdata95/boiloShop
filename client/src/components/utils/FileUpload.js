227fd56667e01733b2335a2fa0608d5e285ca919
mkdata95 <mkdata95@naver.com>
2021-07-20 10:39:56 +0900

    onDrop Fuction make

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import { Icon } from "antd";
+import axios from "axios";
 
 function FileUpload() {
-  return <div>FilesUPload</div>;
+  const [Images, setImages] = useState([]);
+
+  const dropHandler = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+    axios.post("/api/product/image", formData, config).then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setImages([...Images, response.data.filePath]);
+      } else {
+        alert("파일 저장하는데 실패하였습니다.");
+      }
+    });
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Dropzone onDrop={dropHandler}>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            style={{
+              width: 300,
+              height: 240,
+              border: "1px solid lightgray",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            <Icon type="plus" style={{ fontSize: "3rem" }} />
+          </div>
+        )}
+      </Dropzone>
+
+      <div
+        style={{
+          display: "flex",
+          width: "350px",
+          height: "240px",
+          overflow: "scroll",
+        }}
+      >
+        {Images.map((image, index) => (
+          <div key={index}>
+            <img
+              style={{ minWidth: "300px", width: "300px", height: "240px" }}
+              src={`http://localhost:5000/${image} `}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default FileUpload;
